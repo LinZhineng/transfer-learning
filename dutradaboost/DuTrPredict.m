c341768b0030d0e5f25f98a6f3b55090d8e0c6ac
wangmj17 <wangmj17@126.com>
2015-08-28 14:40:55 +0800

    Create

---
+++ dutradaboost/DuTrPredict.m
@@ -0,0 +1,14 @@
+function Ydash = DuTrPredict(X, svmmodels, beta)
+    N = length(svmmodels);
+    start = ceil(N/2);
+    l = size(X,1);
+    yOne = ones(l,1);
+    yTwo = ones(l,1);
+    Ydash = ones(l,1);
+    for i = start:N
+        predict = svmpredict(yOne,X,svmmodels{i});
+        yOne = yOne.*((beta(i)*ones(l,1)).^(-predict));
+        yTwo = yTwo.*((beta(i)*ones(l,1)).^(-0.5));
+    end
+    Ydash(yOne < yTwo) = -1;
+end
